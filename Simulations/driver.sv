62ae12f8df233ea5553814ca9597615bd619e1fb
GhulamMustafa42 <102722177+GhulamMustafa42@users.noreply.github.com>
2022-04-21 11:32:55 -0700

    Update driver.sv

--- Simulations/driver.sv
+++ Simulations/driver.sv
@@ -1,46 +1,43 @@
 
+//`include "transaction.sv"
 
 class driver;
-	//transaction class (Packet class) instance
 	transaction trans;
-	mailbox  mb_gen2driver;
+	mailbox mb_gen2driver;
+	virtual intf vif;
 
-	//contructor
-	function new(mailbox mb_g2d);
-		this.mb_gen2driver = mb_g2d;	
+	//mailbox and virtual interface construct
+	function new(mailbox mb_gen2driver);
+		this.mb_gen2driver = mb_gen2driver;
+	
 	endfunction
 	
-	task get_INCR4();
-		for (int i=0;i<4;i++) begin
-		trans=new;
+	task run ();
+		forever begin 
+		trans = new();
 		mb_gen2driver.get(trans);
-		$display("Address:0x%0h recieved",trans.HADDR);
-		end	
-	endtask
-	
-	task get_INCR8();
-		for (int i=0;i<8;i++) begin
-		trans=new;
-		mb_gen2driver.get(trans);
-		$display("Address:0x%0h recieved",trans.HADDR);
+		$display("Address:%0h Data:%0h",tr.HADDR,tr.HWDATA);
 		end	
 	endtask
 
-	task get_INCR16();
-		for (int i=0;i<16;i++) begin
-		trans=new;
-		mb_gen2driver.get(trans);
-		$display("Address:0x%0h recieved",trans.HADDR);
-		end	
-	endtask
+	task reset(bit HRESTn);
+		if(!HRESTn); //HRESTn is the only active low signal
+  	  	vif.HADDR   <= 32'b0;
+	       	vif.HWDATA  <= 32'b0;
+		vif.HRDATA  <= 32'b0;
+ 	      	vif.HSIZE   <= 3'b010; // Word
+       		vif.HBURST  <= 3'b000; // Single Burst
+     		vif.HTRANS  <= 2'b00;  // IDLE
+     		vif.HWRITE  <= 1'b0;
+   	  	vif.HPROT   <= 4'b0;
+     		vif.HSEL    <= 1'b0;
+		vif.HRESP   <= 1'b0;
+		vif.HREADY  <= 1'b1;
+		vif.HREADYOUT <=1'b1;
+     		$display("Bus System Reset\n");
+       	 	
+	endtask 
 
 
-	task get_SINGLE_BURST(integer beats);
-		for (int i=0;i<beats;i++) begin
-		trans=new;
-		mb_gen2driver.get(trans);
-		$display("Address:0x%0h recieved",trans.HADDR);
-		end	
-	endtask
 
 endclass: driver
