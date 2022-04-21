c1b9ff659ae31bdece651782fea3da783d59c37b
GhulamMustafa42 <102722177+GhulamMustafa42@users.noreply.github.com>
2022-04-21 12:10:58 -0700

    Update generator.sv

--- Simulations/generator.sv
+++ Simulations/generator.sv
@@ -1,148 +1,208 @@
 
+//`include "transaction.sv"
 
 class generator;
 	
-	logic [31:0] addr;
-	logic [31:0] data;
-	logic wr_signal;
+	bit [31:0]addr;
+	bit [31:0]data;
+	bit readwrite;
 
 	transaction trans;
-	mailbox  mb_gen2driver;
+	mailbox mb_gen2driver;
 	
-	//mailbox handler generator to driver
-	function new(mailbox  mb_g2d);	
-		this.mb_gen2driver = mb_g2d;	
+	//mailbox constructor
+	function new(mailbox mb_gen2driver);	
+		this.mb_gen2driver = mb_gen2driver;	
 	endfunction
+
 	
-	task INCR4(bit wr_data);		
-		bit [2:0] add_size;
-		trans=new; 
-		this.trans.HADDR=addr;
-		this.trans.HWDATA=data;
-		this.trans.HBURST = 3'b011; //Incrementing Burst (4-beat)
-       	 	this.trans.HWRITE = wr_data; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
+	task automatic INCR4(bit readwrite);
+		bit check;
+		bit [2:0] index; //increment in addresses
+		bit [2:0] size; //HSIZE
+		trans = new; 
+		
+		this.trans.HWRITE.rand_mode(0);
+		this.trans.HWRITE = readwrite; 
+
+		this.trans.HBURST.rand_mode(0);
+		this.trans.HBURST = 3'b011; //4 beat Incrementing Burst
+		
+		this.trans.HTRANS.rand_mode(0);
+		this.trans.HTRANS= 2'b10; //NONSEQ
+     		
+		check = trans.randomize();	
+		
 		mb_gen2driver.put(trans);
-		$display("Address:0x%0h sent to driver\n",trans.HADDR);
-	 		
+		$display("Address:0x%0h sent to mailbox",trans.HADDR);
+		size = this.trans.HSIZE;
+		addr = this.trans.HADDR;
+		
+
 		repeat(3)	
       			begin
 			trans=new;
-       			this.trans.HWRITE = wr_data; 
-       			this.trans.HBURST = 3'b011; //Incrementing Burst (4-beat)
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = 3'b010; // HSIZE=word (04-bytes)
-      			case(trans.HSIZE)
-				3'b000 : add_size = 1;
-              			3'b001 : add_size = 2;
-          			3'b010 : add_size = 4;
+			this.trans.HADDR.rand_mode(0);
+			this.trans.HADDR=addr;	
+
+			this.trans.HWRITE.rand_mode(0);
+			this.trans.HWRITE = readwrite; 			
+
+			this.trans.HBURST.rand_mode(0);
+			this.trans.HBURST = 3'b011; //4 beat Incrementing Burst
+      			
+			this.trans.HSIZE.rand_mode(0);
+			this.trans.HSIZE = size; 
+
+			check=trans.randomize();
+	
+			case(trans.HSIZE)
+				3'b000 : index = 1;
+              			3'b001 : index = 2;
+          			3'b010 : index = 4;
         		endcase 
-       			mb_gen2driver.put(trans);	
-			addr=addr+add_size;
+
+			mb_gen2driver.put(trans);	
+			addr=addr+index;
 			this.trans.HADDR = addr;
-			$display("Address:0x%0h sent to mailbox",trans.HADDR);
+			$display("Address:0x%0h HSIZE:%0d sent to mailbox",trans.HADDR, trans.HSIZE);
 			end
 	
 		$display("All 4-beats in INCR4 burst sent to driver\n");
 		endtask
 
-
-	task INCR8(bit wr_data);		
-		bit [2:0] add_size;
+	
+	task automatic INCR8(bit readwrite);
+		bit check;
+		bit [2:0] index; //increment in addresses
+		bit [2:0] size; //HSIZE
 		trans=new; 
-		this.trans.HADDR=addr;
-		this.trans.HWDATA=data;
+		
+		this.trans.HWRITE.rand_mode(0);
+		this.trans.HWRITE = readwrite; 
+
+		this.trans.HBURST.rand_mode(0);
 		this.trans.HBURST = 3'b101; //8 beat Incrementing Burst
-       	 	this.trans.HWRITE = wr_data; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
+		
+		this.trans.HTRANS.rand_mode(0);
+		this.trans.HTRANS= 2'b10; //NONSEQ
+     		
+		check=trans.randomize();	
+		
 		mb_gen2driver.put(trans);
-		$display("Address:0x%0h sent to driver\n",trans.HADDR);
-	 		
+		$display("Address:0x%0h sent to mailbox",trans.HADDR);
+		size=this.trans.HSIZE;
+		addr=this.trans.HADDR;
+		
+
 		repeat(7)	
       			begin
 			trans=new;
-       			this.trans.HWRITE = wr_data; 
-       			this.trans.HBURST = 3'b101; //8 beat Incrementing Burst
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = 3'b010; // HSIZE=word (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : add_size = 1;
-              			3'b001 : add_size = 2;
-          			3'b010 : add_size = 4;
+			this.trans.HADDR.rand_mode(0);
+			this.trans.HADDR=addr;	
+
+			this.trans.HWRITE.rand_mode(0);
+			this.trans.HWRITE = readwrite; 			
+
+			this.trans.HBURST.rand_mode(0);
+			this.trans.HBURST = 3'b101; //8 beat Incrementing Burst
+      			
+			this.trans.HSIZE.rand_mode(0);
+			this.trans.HSIZE = size; 
+			
+			check=trans.randomize();
+
+			case(trans.HSIZE)
+				3'b000 : index = 1;
+              			3'b001 : index = 2;
+          			3'b010 : index = 4;
         		endcase 
-       			mb_gen2driver.put(trans);	
-			addr=addr+add_size;
+
+			mb_gen2driver.put(trans);	
+			addr=addr+index;
 			this.trans.HADDR = addr;
-			$display("Address:0x%0h sent to mailbox",trans.HADDR);
+			$display("Address:0x%0h HSIZE:%0d sent to mailbox",trans.HADDR, trans.HSIZE);
 			end
 	
-		$display("All 8-beats in INCR8 burst sent to driver\n");
+		$display("All 8-beats in INCR4 burst sent to driver\n");
 		endtask
 
-	task INCR16(bit wr_data);		
-		bit [2:0] add_size;
-		trans=new; 
-		this.trans.HADDR=addr;
-		this.trans.HWDATA=data;
-		this.trans.HBURST = 3'b111; //16 Beat Incrementing Burst
-       	 	this.trans.HWRITE = wr_data; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
+	task automatic INCR16(bit  readwrite);		
+		bit check;
+		bit [2:0] index; //increment in addresses
+		bit [2:0] size; //HSIZE
+		trans=new;  
+		
+		this.trans.HWRITE.rand_mode(0);
+		this.trans.HWRITE = readwrite; 
+
+		this.trans.HBURST.rand_mode(0);
+		this.trans.HBURST = 3'b111; //16 beat Incrementing Burst
+		
+		this.trans.HTRANS.rand_mode(0);
+		this.trans.HTRANS= 2'b10; //NONSEQ
+     		
+		check=trans.randomize();	
+		
 		mb_gen2driver.put(trans);
-		$display("Address:0x%0h sent to driver\n",trans.HADDR);
-	 		
+		$display("Address:0x%0h sent to mailbox",trans.HADDR);
+		size=this.trans.HSIZE;
+		addr=this.trans.HADDR;
+
 		repeat(15)	
       			begin
 			trans=new;
-       			this.trans.HWRITE = wr_data; 
-       			this.trans.HBURST = 3'b111; //16 Beat Incrementing Burst
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = 3'b010; // HSIZE=word (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : add_size = 1;
-              			3'b001 : add_size = 2;
-          			3'b010 : add_size = 4;
+			this.trans.HADDR.rand_mode(0);
+			this.trans.HADDR=addr;	
+
+			this.trans.HWRITE.rand_mode(0);
+			this.trans.HWRITE = readwrite; 			
+
+			this.trans.HBURST.rand_mode(0);
+			this.trans.HBURST = 3'b111; //16 beat Incrementing Burst
+      			
+			this.trans.HSIZE.rand_mode(0);
+			this.trans.HSIZE = size; 
+			
+			check=trans.randomize();
+
+			case(trans.HSIZE)
+				3'b000 : index = 1;
+              			3'b001 : index = 2;
+          			3'b010 : index = 4;
         		endcase 
-       			mb_gen2driver.put(trans);	
-			addr=addr+add_size;
+
+			mb_gen2driver.put(trans);	
+			addr=addr+index;
 			this.trans.HADDR = addr;
-			$display("Address:0x%0h sent to mailbox",trans.HADDR);
+			$display("Address:0x%0h HSIZE:%0d sent to mailbox",trans.HADDR, trans.HSIZE);
 			end
 	
 		$display("All 16-beats in INCR16 burst sent to driver\n");
 		endtask
 
-
-	task SINGLE_BURST(bit wr_data, integer beats);
-		bit [2:0] add_size;
+	task automatic SINGLE_BURST(bit  readwrite);
+		bit check;
+		bit [2:0] index;
 		trans=new; 
-		this.trans.HADDR=addr;
-		this.trans.HWDATA=data;
-		this.trans.HBURST = 3'b000; //Single Burst
-       	 	this.trans.HWRITE = wr_data; 
-     		this.trans.HTRANS = 3'b010; // Non-Sequential 
-		mb_gen2driver.put(trans);
-		$display("Address:0x%0h sent to driver\n",trans.HADDR);
-	 	beats=beats-1; //because remainign are sequential
-		repeat(beats)	
-      			begin
-			trans=new;
-       			this.trans.HWRITE = wr_data; 
-       			this.trans.HBURST = 3'b000; //Single Burst
-      			this.trans.HTRANS = 3'b011; // SEQ for the remaining beats
-      			this.trans.HSIZE = 3'b010; // HSIZE=word (32-bits)
-      			case(trans.HSIZE)
-				3'b000 : add_size = 1;
-              			3'b001 : add_size = 2;
-          			3'b010 : add_size = 4;
-        		endcase 
-       			mb_gen2driver.put(trans);	
-			addr=addr+add_size;
-			this.trans.HADDR = addr;
-			$display("Address:0x%0h sent to mailbox",trans.HADDR);
-			end
-	
-		$display("All beats in Single Burst sent to driver\n");
-	endtask
+		
+		this.trans.HWRITE.rand_mode(0);
+		this.trans.HWRITE = readwrite; 
 
+		this.trans.HBURST.rand_mode(0);
+		this.trans.HBURST = 3'b000; //Single Burst
+		
+		this.trans.HTRANS.rand_mode(0);
+		this.trans.HTRANS= 2'b10; //NONSEQ
+     		
+		check=trans.randomize();	
+	       	 		
+		mb_gen2driver.put(trans);
+
+		$display("Address:0x%0h sent to mailbox\n",trans.HADDR);
+		
+		$display("Single Burst sent to driver\n");
+		endtask
+	
 
 endclass: generator
